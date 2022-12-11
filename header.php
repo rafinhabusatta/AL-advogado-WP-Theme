1e127d98f768105e06025d538924972813d8b87e
rafinhabusatta <lacerdabusatta@gmail.com>
2022-12-11 20:20:06 -0300

    Header Wordpress - cópia do html

--- header.php
+++ header.php
@@ -8,4 +8,54 @@
   <title><?php the_title(); ?></title>
 </head>
 <body>
+<header>
+      <nav class="navbar navbar-dark navbar-expand-lg bg-blue">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="assets/Logo.svg" alt="Logo" height="100" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sobre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+              </li>
+            </ul>
+            <!-- <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Pesquisar"
+                aria-label="Pesquisar"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Pesquisar
+              </button>
+            </form> -->
+          </div>
+        </div>
+      </nav>
+      <div class="header-banner">
+        <h1 class="text-gold fw-bold mx-3 fs-32">
+          Há 20 anos garantindo os direitos previdenciários dos cidadãos
+          brasileiros
+        </h1>
+      </div>
+    </header>
   
