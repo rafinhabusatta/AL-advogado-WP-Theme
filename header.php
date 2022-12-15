d413837b7c13cf6733a219ee2c3b681f9b946df7
rafinhabusatta <lacerdabusatta@gmail.com>
2022-12-15 15:33:35 -0300

    importação de css e js bootstrap através de funções do wordpress; Separação do css e importação por funções do wordpress; modificação da index.php;

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <header>
-<nav class="navbar navbar-dark navbar-expand-lg bg-blue">
+  <nav class="navbar navbar-dark navbar-expand-lg bg-blue">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <img src="assets/Logo.svg" alt="Logo" height="100" />
@@ -51,26 +51,22 @@
       </div>
     </div>
   </nav>
-  <div class="header-banner bg-blue">
-    <!-- <div class="row overflow-hidden"> -->
-      <!-- <div class=""> -->
-        <h1 class="text-gold fw-bold mx-3 fs-32 g-column-1-span-2">
+  <div class="header-banner text-center text-lg-start bg-blue">
+    <div class="row w-100">
+      <div class="col-12 col-lg-6">
+        <h1 class="text-gold fw-bold mx-3 fs-32 fs-lg-65">
           Há 20 anos garantindo os direitos previdenciários dos cidadãos
           brasileiros
         </h1>
-        <button type="button" class="btn btn-dark btn-al-advogados g-column-1 g-row-2">Faça uma consulta gratuita</button>
-        <button type="button" class="btn btn-dark btn-al-advogados g-column-2 g-row-2">Entre em contato</button>
-
-        <img src="assets/SVG/themis.svg" alt="Advogado" height="400px" />
-        <!-- <button type="button" class="btn btn-gold">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
-          </svg>
-          Whatsapp
-        </button>
-        <button type="button" class="btn btn-gold">Email</button> -->
-      <!-- </div> -->
-    <!-- </div> -->
+        <button type="button" class="btn btn-dark btn-al-advogados mt-3 mt-lg-5">Faça uma consulta gratuita</button>
+        <button type="button" class="btn btn-dark btn-al-advogados mt-3 mt-lg-5">Entre em contato</button>
+        <div class="row mt-5 pt-0 pt-lg-5">
+          <div class="col-3"><p>15K</p><span>casos ganhos</span></div>
+          <div class="col-3"><p>15K</p><span>casos ganhos</span></div>  
+        </div>
+      </div>
+      <div class="col-lg-6 themis"></div>
+    </div>
   </div>
 </header>
   
--- index.php
+++ index.php
@@ -1,11 +1,154 @@
-<?php get_header(); 
-
- while(have_posts()) {
-  the_post(); ?>
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  
-  <?php the_content(); ?>
-  <hr>
-  <?php }
-  get_footer();
-?>
+<?php get_header();  ?>
+<main class="container">
+  <div class="row mt-5">
+    <div class="col-12">
+      <h2 class="fs-32 fs-lg-48 fw-semibold mb-4 position-relative">
+        Sobre Nós<span class="shadow-text">Sobre Nós</span>
+      </h2>
+      <p class="text-justify">
+        Advogado com formação Acadêmica na Pontifícia Universidade Católica
+        de Porto Alegre-RS no ano de 1998, Carteira da OAB/RS 54.981 desde o
+        ano de 2002, atuando em conjunto com a Doutora Elisabete Maria Vaz
+        de Andrade, nos anos de 2003 a maio de 2013, na cidade de Porto
+        Alegre-RS. Possui especialização nas áreas de Direito do Trabalho e
+        Direito Previdenciário -Aposentadorias e Pensões do INSS, iniciando
+        a sua atuação também na cidade de Caxias do Sul, associado do
+        Instituto Brasileiro de Direito Previdenciário, uma associação civil
+        de cunho científico-jurídico, com foco na defesa do Direito
+        Previdenciário e por decorrência dos Aposentados e Pensionistas do
+        Regime Geral da Previdência Social. O estudo permanente em busca de
+        soluções para as aposentadorias e pensões dos trabalhadores
+        brasileiros.
+      </p>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col-12">
+      <h2 class="fs-32 fs-lg-48 fw-semibold mb-4 position-relative">
+        Especialidades<span class="shadow-text">Especialidades</span>
+      </h2>
+      <div class="row row-cols-1 row-cols-md-3 g-4 text-center">
+        <div class="col">
+          <div class="card bg-blue text-white h-100">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">
+                Aposentadorias e Planejamento
+              </h5>
+              <p class="card-text">
+                Aposentadoria rural e urbana, por idade, tempo de
+                contribuição, deficiência, professor, perculosidade e
+                insalubridade, e planejamento de aposentadoria.
+              </p>
+            </div>
+            <div class="card-footer bg-blue border-top-0">
+                <a href="#" class="btn btn-gold">Saiba mais</a>
+              </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card bg-blue text-white h-100">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">Pensões</h5>
+              <p class="card-text">
+                Pensões por morte rural e urbana, especial por hanseníase,
+                síndrome da Talidomida e crianças com Síndrome Congênita do
+                Zika Vírus.
+              </p>
+            </div>
+            <div class="card-footer bg-blue border-top-0">
+                <a href="#" class="btn btn-gold">Saiba mais</a>
+              </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card bg-blue text-white h-100">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">Auxílios</h5>
+              <p class="card-text">
+                Acidente, doença, reclusão rural e urbana, acidente de
+                trabalho.
+              </p>
+            </div>
+            <div class="card-footer bg-blue border-top-0">
+                <a href="#" class="btn btn-gold">Saiba mais</a>
+              </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card bg-blue text-white h-100">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">Rural</h5>
+              <p class="card-text">
+                lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum
+              </p>
+            </div>
+            <div class="card-footer bg-blue border-top-0">
+                <a href="#" class="btn btn-gold">Saiba mais</a>
+              </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card bg-blue text-white h-100">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">Interdição - Curador 
+                de pessoa incapaz</h5>
+              <p class="card-text">
+                lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum
+              </p>
+            </div>
+            <div class="card-footer bg-blue border-top-0">
+                <a href="#" class="btn btn-gold">Saiba mais</a>
+              </div>
+          </div>
+        </div>
+        <div class="col"><div class="card bg-blue text-white h-100">
+          <div class="card-body">
+            <h5 class="card-title fw-bold">IBenefícios Assistenciais</h5>
+            <p class="card-text">
+              LOAS/BPC, idoso, deficiência, trabalhador portuário avulso, reativação de benefício suspenso.
+            </p>
+            <div class="card-footer bg-blue border-top-0">
+              <a href="#" class="btn btn-gold">Saiba mais</a>
+            </div>
+          </div></div>
+      </div>
+    </div>
+  </div>
+  <div class="row mt-5 bg-blue-light">
+    <div class="col-12 col-lg-6">
+      <h2 class="fs-32 fs-lg-48 fw-semibold mb-4">Atendemos todo
+        país remotamente!</h2>
+        <p class="text-justify">
+          Estamos localizados na praça osvaldo cruz 15, na sala 1905, prontos para atender você! Também estamos disponível online através do whatsapp.
+        </p>
+        <button type="button" class="btn btn-dark">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+          </svg>
+          Whatsapp
+        </button>
+    </div>
+    <div class="col-12 col-6">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d863.5915287956304!2d-51.2228087!3d-30.0263527!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519791d64f85901%3A0xc56f8460c312c4f3!2sCondom%C3%ADnio%20Edif%C3%ADcio%20Coliseu!5e0!3m2!1spt-BR!2sbr!4v1670796572077!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col-12 col-lg-6">
+      <h2 class="fs-32 fw-bold mb-4 text-blue">PLANEJE A SUA APOSENTADORIA CONOSCO</h2>
+        <p class="text-justify">
+          Fazemos um estudo de projeção financeira para que você possa aproveitar sua aposentadoria ao máximo
+        </p>
+        <button type="button" class="btn btn-dark">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+          </svg>
+          Whatsapp
+        </button>
+    </div>
+    <div class="d-none d-lg-block col-6">
+      <img class="img-fluid" src="assets/SVG/investing.svg" alt="Ilustração">
+  </div>
+</main>
+<?php  get_footer(); ?>
