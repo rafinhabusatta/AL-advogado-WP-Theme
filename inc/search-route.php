996a4e60bb1d698e4c9f54c3e9e888bcc1a630ca
rafinhabusatta <lacerdabusatta@gmail.com>
2023-01-10 14:32:13 -0300

    remoção de dependência jquery para a busca; atualização do layout da busca

--- inc/search-route.php
+++ inc/search-route.php
@@ -1,28 +1,50 @@
 <?php 
 
 function aladvogadosRegisterSearch() {
-  register_rest_route('aladvogados/v1', 'search', array(
+  register_rest_route('al-advogados/v1', 'search', array(
     'methods' => WP_REST_SERVER::READABLE, // GET request (crud)
     'callback' => 'aladvogadosSearchResults'
   )); // namespace, route, array of options
 }
 add_action('rest_api_init', 'aladvogadosRegisterSearch');
 
-function aladvogadosSearchResults() {
-  $professors = new WP_QUERY(array(
-    'post_type' => 'professor'
+function aladvogadosSearchResults($dataSearch) {
+  $mainQuery = new WP_QUERY(array(
+    'post_type' => array('post', 'page', 'especialidade', 'beneficio'),
+    's' => sanitize_text_field($dataSearch['term'])
   ));
 
-  $professorResults = array();
+  $results = array(
+    'generalInfo' => array(),
+    'beneficios' => array(),
+    'especialidades' => array(),
+  );
 
-  while($professors->have_posts()) {
-    $professors->the_post();
-    array_push($professorResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink()
-    
-    ));
+  while($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+
+    if (get_post_type() == 'post' OR get_post_type() == 'page') {
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink() ,
+        'postType' => get_post_type(),
+        'authorName' => get_the_author()
+      ));
+    }
+
+    if (get_post_type() == 'especialidade') {
+      array_push($results['especialidades'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'benefcio') {
+      array_push($results['beneficios'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink() 
+      ));
+    }    
   }
-  
-  return $professorResults;
+  return $results;
 }
